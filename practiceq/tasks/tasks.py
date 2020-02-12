74c55a5632f8831a7d927ec8e0e0f1e6a0465829
Nithivarn-Reddy <nithivarn.reddy.shanigaram-1@ou.edu>
2020-02-12 08:37:40 -0600

    image process added

--- practiceq/tasks/tasks.py
+++ practiceq/tasks/tasks.py
@@ -177,6 +177,61 @@ def automate():
     result = chain(getSample.s(),derivative_generation.s())
     result.delay()
     return "automate kicked off"
+
+def _processimage(inpath, outpath, outformat="TIFF", filter="ANTIALIAS", scale=None, crop=None):
+    """
+    Internal function to create image derivatives
+    """
+
+    try:
+        image = Image.open(inpath)
+    except (IOError, OSError):
+        # workaround for Pillow not handling 16bit images
+        if "16-bit" in check_output(("identify", inpath)):
+            with NamedTemporaryFile() as tmpfile:
+                check_call(("convert", inpath, "-depth", "8", tmpfile.name))
+                image = Image.open(tmpfile.name)
+        else:
+            raise Exception
+
+    if crop:
+        image = image.crop(crop)
+
+    if scale:
+        imagefilter = getattr(Image, filter.upper())
+        size = [x * scale for x in image.size]
+        image.thumbnail(size, imagefilter)
+
+    image.save(outpath, outformat)
+
+@task()
+def processimage(inpath, outpath, outformat="TIFF", filter="ANTIALIAS", scale=None, crop=None):
+    """
+    Digilab TIFF derivative Task
+    args:
+      inpath - path string to input image
+      outpath - path string to output image
+      outformat - string representation of image format - default is "TIFF"
+      scale - percentage to scale by represented as a decimal
+      filter - string representing filter to apply to resized image - default is "ANTIALIAS"
+      crop - list of coordinates to crop from - i.e. [10, 10, 200, 200]
+    """
+
+    task_id = str(processimage.request.id)
+    #create Result Directory
+    #resultpath = os.path.join(basedir, 'oulib_tasks/', task_id)
+    #os.makedirs(resultpath)
+
+    _processimage(inpath=inpath,
+                  outpath=outpath,
+                  outformat=outformat,
+                  filter=filter,
+                  scale=scale,
+                  crop=crop
+                  )
+
+    return "{0}/oulib_tasks/{1}".format(hostname, task_id)
+
 @task
 def read():
     """
@@ -184,8 +239,12 @@ def read():
     source = source file.
 
     """
-    path = '/mnt/{0}/{1}/*'.format("source","Abbati_1703")
+    path = '/mnt/{0}/{1}/data/*.tif'.format("source","Abbati_1703")
+    outpath = '/mnt/{0}/{1}/data/'.format("derivative","Abbati_1703")
+    for file in glob.glob(path):
+        processimage(inpath=file,outpath=outpath)
 
-    return glob.glob(path)
+
+
 
 

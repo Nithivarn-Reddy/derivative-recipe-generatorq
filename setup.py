936e0272a042376181cd8282a296faeba7678e9b
Nithivarn-Reddy <nithivarn.reddy.shanigaram-1@ou.edu>
2020-08-24 13:04:25 -0500

    renamed files

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #import ez_setup
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
-setup(name='practiceq',
+setup(name='derivative-recipe-generatorq',
       version='0.0.0',
       packages= find_packages(),
       install_requires=[
